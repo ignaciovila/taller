f3806631b7206338baa8b460a0ff18b11121b2b3
Vila <ignacio.vilagraca@mercadolibre.com>
2021-12-16 08:49:32 -0300

    refactor

--- src/main/java/com/mercadolibre/dojos/Main.java
+++ src/main/java/com/mercadolibre/dojos/Main.java
@@ -7,17 +7,17 @@ class Main {
         List<Integer[]> result = new ArrayList<>();
 
         for (int i = 0; i < array.length; i++) {
-            for (int j = 1; j < array.length; j++ ) {
+            for (int j = 1; j < array.length; j++) {
                 for (int k = 2; k < array.length; k++) {
                     for (int l = 3; l < array.length; l++) {
-                        List<Integer> candidates = List.of(i, j, k, l);
-                        Integer[] integers = new Integer[]{array[i], array[j], array[k], array[l]};
+                        if (array[i] + array[j] + array[k] + array[l] == targetSum) {
+                            List<Integer> candidates = List.of(i, j, k, l);
+                            Integer[] integers = new Integer[]{array[i], array[j], array[k], array[l]};
 
-                        boolean unique = candidates.stream().distinct().count() == 4;
-                        boolean visited = result.stream().anyMatch(r -> List.of(r).containsAll(List.of(integers)));
+                            boolean unique = candidates.stream().distinct().count() == 4;
+                            boolean visited = result.stream().anyMatch(r -> List.of(r).containsAll(List.of(integers)));
 
-                        if (unique && !visited) {
-                            if (array[i] + array[j] + array[k] + array[l] == targetSum) {
+                            if (unique && !visited) {
                                 result.add(integers);
                             }
                         }
